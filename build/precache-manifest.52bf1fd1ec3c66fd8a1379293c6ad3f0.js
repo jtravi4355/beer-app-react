ea893fbc667d8cc8781dbf275e418985caf6a2e5
jtravi4355 <jrtravi43@gmail.com>
2020-07-09 18:55:09 -0400

    build

---
+++ build/precache-manifest.52bf1fd1ec3c66fd8a1379293c6ad3f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce180b71c2907a0fab5990b5120229f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3cb9aae59172aae13247",
+    "url": "/static/css/main.553d0da7.chunk.css"
+  },
+  {
+    "revision": "c8af6314a30799574c1b",
+    "url": "/static/js/2.1fc1e36e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1fc1e36e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cb9aae59172aae13247",
+    "url": "/static/js/main.e03be9ed.chunk.js"
+  },
+  {
+    "revision": "35beb3dd3ab4350b7cc9",
+    "url": "/static/js/runtime-main.0bbfb445.js"
+  }
+]);
